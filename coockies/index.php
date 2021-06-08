f780701527d4f4ce3ffc97d464f46111271de46e
juank2808 <webjcaf022014@gmail.com>
2021-06-08 15:41:16 +0200

    trying with cookies

---
+++ coockies/index.php
@@ -0,0 +1,14 @@
+<?php
+setcookie('font-size','30px', time() + 60 * 60 * 24 * 30,'/');
+ ?>
+
+ <!DOCTYPE html>
+ <html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <title>COOCKIES</title>
+</head>
+<body>
+  <h1>My Cookie</h1>
+</body>
+</html>
