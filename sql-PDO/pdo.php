3ef3afb102f53ac4eb7cfad36e8fe141e9c1c59f
juank2808 <webjcaf022014@gmail.com>
2021-06-09 17:22:02 +0200

    SQL Query

--- sql-PDO/pdo.php
+++ sql-PDO/pdo.php
@@ -3,6 +3,7 @@
 try {
   $connect = new PDO('mysql:host=127.0.0.1;dbname=PDO-php','root','');
   echo "Done!";
+  $res = $connect->query('SELECT * from users');
 }catch(PDOException $e){
   echo "ERROR: ".$e->getMessage();
 }
