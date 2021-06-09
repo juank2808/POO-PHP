37f8551596abf57913dd371636622e0aa4143f4a
juank2808 <webjcaf022014@gmail.com>
2021-06-09 17:47:58 +0200

    SQL prepare statement

--- sql-PDO/pdo.php
+++ sql-PDO/pdo.php
@@ -1,9 +1,24 @@
 <?php
-
+$id = $_GET['id'];
 try {
   $connect = new PDO('mysql:host=127.0.0.1;dbname=PDO-php','root','');
-  echo "Done!";
-  $res = $connect->query('SELECT * from users');
+  echo "Done! <br/>";
+  // $res = $connect->query('SELECT * from users');
+  //
+  // foreach ($res as $row) {
+  //   var_dump($row['userName']);
+  // }
+  $sta = $connect->prepare('SELECT * FROM users WHERE id = :id');
+  $sta->execute(
+    array(':id'=>$id)
+  );
+  $res = $sta->fetch();
+  var_dump($res);
+  // To show all
+  // $res = $sta->fetchAll();
+  // foreach ($res as $row) {
+  //   var_dump($row['name']);
+  // }
 }catch(PDOException $e){
   echo "ERROR: ".$e->getMessage();
 }
