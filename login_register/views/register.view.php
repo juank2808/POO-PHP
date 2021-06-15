2f728268a33898cb6dec461568c4456f61f55781
juank2808 <webjcaf022014@gmail.com>
2021-06-15 11:01:20 +0200

    Prearing form and style CSS too

--- login_register/views/register.view.php
+++ login_register/views/register.view.php
@@ -6,13 +6,24 @@
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
     <link href="https://fonts.googleapis.com/css2?family=Raleway&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
-    <title></title>
+    <title>Register</title>
   </head>
   <body>
     <div class="container">
       <h1 class="title">Register</h1>
       <hr class="border"/>
-      
+      <form class="form" name="login" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
+        <div class="form-group">
+          <i class="icon left fa fa-user"></i><input type="text" name="user" value="" class="user" placeholder="New user">
+        </div>
+        <div class="form-group">
+          <i class="icon left fa fa-lock"></i><input type="password" name="password" value="" class="password" placeholder="New password">
+        </div>
+        <div class="form-group">
+          <i class="icon left fa fa-lock"></i><input type="password" name="password2" value="" class="pass_btn" placeholder="Sec password">
+          <i class="send-btn fa fa-arrow-right"></i>
+        </div>
+      </form>
     </div>
   </body>
 </html>
