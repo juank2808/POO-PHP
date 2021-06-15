f0b667b22c9054d9b476ca80d0d81d222876852a
juank2808 <webjcaf022014@gmail.com>
2021-06-15 13:09:16 +0200

    Ending hashing. validadion and we can create users

--- login_register/register.php
+++ login_register/register.php
@@ -6,12 +6,12 @@ if (isset($_SESSION['user'])) {
   header('Location: index.php');
 }
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-  $user = filter_var(mb_strtolower($_POST['user']), FILTER_SANITIZE_STRING);
+  $user = filter_var(strtolower($_POST['user']), FILTER_SANITIZE_STRING);
   $pass = $_POST['password'];
   $pass2 = $_POST['password2'];
 
   $error = '';
-
+$statement = '';
   if (empty($user) or empty($pass) or empty($pass2)) {
     $error.= '<li>Please, write the data correctly!</li>';
   }else {
@@ -19,10 +19,26 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $statement->execute(array(':user'=>$user));
 
     $result = $statement->fetch();
-    // print_r($result);
+
     if ($result !=false) {
-      $error.='The user '.$user.' already exists !';
+      $error.='<li>The user '.$user.' already exists! </li>';
     }
+
+    $pass = hash('sha512', $pass);
+    $pass2 = hash('sha512', $pass2);
+
+    if ($pass != $pass2) {
+      $error.= "<li> The passwords do not match! </li>";
+    }
+  }
+  if ($error == '') {
+    $statement = $conn->prepare(
+      "INSERT INTO users (id,userName, u_password) VALUES (null, :user, :password)");
+
+      $statement->execute(array(":user"=> $user,
+                                ":password"=> "$pass"));
+
+      header('Location: login.php');
   }
 }
 require('views/register.view.php');
