5d5867aadf3173fe02ba0d9b204618a4c3c30f96
juank2808 <webjcaf022014@gmail.com>
2021-06-15 11:55:50 +0200

    Adding log in form

---
+++ login_register/views/login.view.php
@@ -0,0 +1,30 @@
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, user-scalable=nom initial-scale=1.0; minumum-scale=1.0">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <link href="https://fonts.googleapis.com/css2?family=Raleway&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Log in</title>
+  </head>
+  <body>
+    <div class="container">
+      <h1 class="title">Log in</h1>
+      <hr class="border"/>
+      <form class="form" name="login" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
+        <div class="form-group">
+          <i class="icon left fa fa-user"></i><input type="text" name="user" value="" class="user" placeholder="New user">
+        </div>
+        <div class="form-group">
+          <i class="icon left fa fa-lock"></i><input type="password" name="password" value="" class="pass_btn" placeholder="Password">
+          <i class="send-btn fa fa-arrow-right" onclick="login.submit();"></i>
+        </div>
+      </form>
+      <p class="text-register">
+        Don't have an account?
+        <a href="register.php">Join now</a>
+      </p>
+    </div>
+  </body>
+</html>
