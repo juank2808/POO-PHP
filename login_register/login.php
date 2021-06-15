3348007a2cc539b30b5e50a721aa0bc22c0b04c4
juank2808 <webjcaf022014@gmail.com>
2021-06-15 13:59:39 +0200

    Checking login, adding logout, protect the content

--- login_register/login.php
+++ login_register/login.php
@@ -8,23 +8,26 @@ if (isset($_SESSION['user'])) {
   header('Location: index.php');
 }
 
-if ($_SERVER[REQUEST_METHOD] == 'POST') {
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   $user = filter_var(strtolower($_POST['user']),FILTER_SANITIZE_STRING);
   $pass = $_POST['password'];
   $pass = hash('sha512', $pass);
 
-  $statement = $conn->prepare('SELECT * users WHERE userName=:user and u_password =:pass');
+  $statement = $conn->prepare('SELECT * FROM users WHERE userName=:user and u_password =:pass');
 
   $statement->execute(array(
     ':user'=>$user,
     ':pass'=>$pass
   ));
+
   $result= $statement->fetch();
-  if ($result == false) {
-    $error.="<li>The user or password are incorrect</li>"
-  }else{
+  
+  var_dump($result);
+  if ($result !== false) {
     $_SESSION['user']=$user;
     header('Location: index.php');
+  }else{
+    $error.="<li>The user or password are incorrect</li>";
   }
 }
 
