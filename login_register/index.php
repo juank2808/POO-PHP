84f029e1219c18e941fa1bbcc37cffeb7713c5a7
juank2808 <webjcaf022014@gmail.com>
2021-06-15 10:35:51 +0200

    Preparing code

---
+++ login_register/index.php
@@ -0,0 +1,8 @@
+<?php session_start();
+
+if (isset($_SESSION['user'])) {
+  header('Location: content.php');
+}else{
+  header('Location: register.php');
+}
+?>
---
+++ login_register/views/register.view.php
@@ -0,0 +1,18 @@
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, user-scalable=nom initial-scale=1.0; minumum-scale=1.0">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <link href="https://fonts.googleapis.com/css2?family=Raleway&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+    <title></title>
+  </head>
+  <body>
+    <div class="container">
+      <h1 class="title">Register</h1>
+      <hr class="border"/>
+      
+    </div>
+  </body>
+</html>
